ace7d97c27240f49222a388d458f6e2c0ff0bd32
Danii <dpshestakov@edu.hse.ru>
2026-03-05 21:17:42 +0300

    fix bugs with, and isolate embedding

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .llm_client import acall_llm_json, acall_llm_text
 from .preprocessing import format_chat_message
 from .state_logger import init_logs_dir, log_pydantic, log_dict, log_text, log_graphml
+from .embeddings import aget_embeddings_safe
